105a7a0170e9473372c5b8bb4021b2effca5b212
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-15 12:24:49 -0700

    Update type requirements and implement basic toggle for summary and detail view

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -5,14 +5,21 @@ import ListElement from './List/ListElement.jsx';
 
 const List = ({ list }) => {
   return (
-    <div>
-      {list.map((element) => <ListElement key={element.data[0].nasa_id} element={element} />)}
+    <div id='app-list'>
+      {list.map((element) => (
+        <ListElement
+          key={element.data[0].nasa_id}
+          element={element.data[0]}
+        />
+      ))}
     </div>
   );
 };
 
 List.propTypes = {
-  list: PropTypes.arrayOf(PropTypes.object).isRequired,
+  list: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
 };
 
 export default List;
--- client/src/components/List/ListElement.jsx
+++ client/src/components/List/ListElement.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default ({ props }) => {
+import DetailView from './DetailView.jsx';
+import SummaryView from './SummaryView.jsx';
+
+const ListElement = ({ element }) => {
+  const [isSummaryView, setView] = useState(true);
+
+  if (isSummaryView) {
+    return (
+      <div
+        aria-hidden
+        onClick={() => setView(!isSummaryView)}
+        onKeyPress={() => setView(!isSummaryView)}
+        role='button'
+      >
+        <SummaryView element={element} />
+      </div>
+    );
+  }
   return (
-    <div>
-      ListElement
+    <div
+      aria-hidden
+      onClick={() => setView(!isSummaryView)}
+      onKeyPress={() => setView(!isSummaryView)}
+      role='button'
+    >
+      <DetailView
+        element={element}
+      />
     </div>
   );
 };
+
+ListElement.propTypes = {
+  element: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.arrayOf(PropTypes.string),
+    ]),
+  ).isRequired,
+};
+
+export default ListElement;
