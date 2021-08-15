3c0c7c37d515e4bded6cbb9e72e5f267928a0edd
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-15 09:37:50 -0700

    Implement basic app file structure

---
+++ client/src/config/config.js
@@ -0,0 +1,5 @@
+/*
+  Normally this would not be tracked in git, and would probably be on the server so keys aren't exposed via the client.
+  However, this particular key is not strictly required and is easy to get via https://api.nasa.gov/.
+*/
+module.exports = 'G1YGWrHAmVHpbNzqBehd8CkBTc6Q6UZ8SSzTRar1'
---
+++ client/src/lib/NASA.js
@@ -0,0 +1,19 @@
+/*
+Documentation: https://images.nasa.gov/docs/images.nasa.gov_api_docs.pdf
+
+https://api.nasa.gov/planetary/apod?api_key=G1YGWrHAmVHpbNzqBehd8CkBTc6Q6UZ8SSzTRar1
+*/
+const axios = require('axios');
+
+const BASE_URL = 'https://images-api.nasa.gov/search?'
+
+const NASA = axios.create({
+  baseURL: BASE_URL
+  headers: {
+    common: {
+      'Content-Type': 'application/json',
+    }
+  }
+})
+
+module.exports = NASA;
